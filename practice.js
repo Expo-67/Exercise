268a5c6b4445732fa2ae35a37f568efc757962f4
Expo-67 <145459052+Expo-67@users.noreply.github.com>
2024-06-20 16:51:27 +0300

    third commit

--- practice.js
+++ practice.js
@@ -11,22 +11,22 @@ const student2 = {
 const student3 = {
   firstName: "Diana",
   lastName: "Prince",
-  age: 38,
+  age: 78,
 };
 function studentGeneration(student) {
   const { age } = student;
-}
 
-if (student.age > 18 && student.age <= 35) {
-  console.log(`${student.firstName} ${student.lastName} Gen Z.`);
-} else if (student.age > 36 && student.age <= 42) {
-  console.log(`${student.firstName} ${student.lastName}  Millennial.`);
-} else if (student.age > 43 && student.age <= 70) {
-  console.log(`${student.firstName} ${student.lastName} Boomer.`);
-} else {
-  console.log(
-    `${student.firstName} ${student.lastName} generation before Boomers.`
-  );
+  if (student.age > 18 && student.age <= 35) {
+    console.log(`${student.firstName} ${student.lastName} Gen Z.`);
+  } else if (student.age > 36 && student.age <= 42) {
+    console.log(`${student.firstName} ${student.lastName}  Millennial.`);
+  } else if (student.age > 43 && student.age <= 70) {
+    console.log(`${student.firstName} ${student.lastName} Boomer.`);
+  } else {
+    console.log(
+      `${student.firstName} ${student.lastName} generation before Boomers.`
+    );
+  }
 }
 
 studentGeneration(student);
